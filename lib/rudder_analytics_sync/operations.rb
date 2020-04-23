5e8e33664e5baf31261a4e84a93c31034fd29884
arnab <arnab@rudderlabs.com>
2020-04-24 00:49:52 +0530

    Added screen dependency

--- lib/rudder_analytics_sync/operations.rb
+++ lib/rudder_analytics_sync/operations.rb
@@ -5,6 +5,7 @@ require 'rudder_analytics_sync/operations/operation'
 require 'rudder_analytics_sync/operations/identify'
 require 'rudder_analytics_sync/operations/track'
 require 'rudder_analytics_sync/operations/page'
+require 'rudder_analytics_sync/operations/screen'
 require 'rudder_analytics_sync/operations/group'
 require 'rudder_analytics_sync/operations/alias'
 
